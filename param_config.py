fa618d3f0fa957b2451feb57d08bec3c1cc12f62
Fan Min at swpu.edu.cn <30546261+FanSmale@users.noreply.github.com>
2023-12-01 22:27:40 +0800

    Update param_config.py

--- param_config.py
+++ param_config.py
@@ -2,9 +2,9 @@
 """
 Parameters setting
 
-Created on Sep 2023
+Created on Feb 2023
 
-@author: Xing-Yi Zhang (zxy20004182@163.com)
+@author: Xing-Yi Zhang (Zhangzxy20004182@163.com)
 
 """
 
@@ -32,6 +32,7 @@ if dataset_name  == 'SEGSimulation':
     firststage_epochs = 40
     secondstage_epochs = 30
     thirdstage_epochs = 30
+    loss_weight = [1, 1e6]
     epochs = firststage_epochs + secondstage_epochs + thirdstage_epochs
 
     train_batch_size = 10                           # Number of batches fed in network in one training epoch.
@@ -41,78 +42,82 @@ elif dataset_name  == 'SEGSalt':
     data_dim = [400, 301]
     model_dim = [201, 301]
     inchannels = 29
-    train_size = 130
+    train_size = 30
     test_size = 10
 
     firststage_epochs = 0
     secondstage_epochs = 0
     thirdstage_epochs = 50                          # SEGSalt for transfer learning and does not require curriculum tasks
+    loss_weight = [1, 1e6]
     epochs = firststage_epochs + secondstage_epochs + thirdstage_epochs
 
-    train_batch_size = 10
+    train_batch_size = 5
     test_batch_size = 2
 
 elif dataset_name == 'FlatVelA':
     data_dim = [1000, 70]
     model_dim = [70, 70]
     inchannels = 5
-    train_size = 5000
-    test_size = 1000
+    train_size = 24000
+    test_size = 6000
 
-    firststage_epochs = 30
-    secondstage_epochs = 20
-    thirdstage_epochs = 50
+    firststage_epochs = 10
+    secondstage_epochs = 10
+    thirdstage_epochs = 120
+    loss_weight = [1, 0.01]
     epochs = firststage_epochs + secondstage_epochs + thirdstage_epochs
 
-    train_batch_size = 50
+    train_batch_size = 64
     test_batch_size = 5
 
 elif dataset_name == 'CurveVelA':
     data_dim = [1000, 70]
     model_dim = [70, 70]
     inchannels = 5
-    train_size = 5000
-    test_size = 1000
+    train_size = 24000
+    test_size = 6000
 
     firststage_epochs = 10
     secondstage_epochs = 10
-    thirdstage_epochs = 80
+    thirdstage_epochs = 100
+    loss_weight = [1, 0.1]
     epochs = firststage_epochs + secondstage_epochs + thirdstage_epochs
 
-    train_batch_size = 50
+    train_batch_size = 64
     test_batch_size = 5
 
 elif dataset_name == 'FlatFaultA':
     data_dim = [1000, 70]
     model_dim = [70, 70]
     inchannels = 5
-    train_size = 5000
-    test_size = 1000
+    train_size = 48000
+    test_size = 6000
 
     firststage_epochs = 10
     secondstage_epochs = 10
-    thirdstage_epochs = 80
+    thirdstage_epochs = 100
+    loss_weight = [1, 0.01]
     epochs = firststage_epochs + secondstage_epochs + thirdstage_epochs
 
-    train_batch_size = 50
+    train_batch_size = 64
     test_batch_size = 5
 
 elif dataset_name == 'CurveFaultA':
     data_dim = [1000, 70]
     model_dim = [70, 70]
     inchannels = 5
-    train_size = 5000
-    test_size = 1000
+    train_size = 48000
+    test_size = 6000
 
-    firststage_epochs = 30
-    secondstage_epochs = 20
-    thirdstage_epochs = 50
+    firststage_epochs = 10
+    secondstage_epochs = 10
+    thirdstage_epochs = 100
+    loss_weight = [1, 0.1]
     epochs = firststage_epochs + secondstage_epochs + thirdstage_epochs
 
-    train_batch_size = 50
+    train_batch_size = 64
     test_batch_size = 5
 
 else:
     print('The selected dataset is invalid')
     exit(0)
-
